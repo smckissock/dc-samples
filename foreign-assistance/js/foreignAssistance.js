325778d1f1dc20ca0686bbcb33c13e5ebc8b8c69
Scott McKissock <smckissock@gmail.com>
2014-06-14 19:09:25 -0400

    Added function to make row charts

--- foreign-assistance/js/foreignAssistance.js
+++ foreign-assistance/js/foreignAssistance.js
@@ -10,45 +10,14 @@ d3.csv("data/foreignAssistance.csv", function (data) {
         if (d.sector == "NULL")
             d.sector = "Unspecified";
     });
-    console.log(data);
-
     var facts = crossfilter(data);
-    console.log(facts);
-
+   
+    var lightBlue = ['#9ecae1'];
+    
     var fiscalYearDim = facts.dimension(function (d) { return d.fiscalYear; });
     var fiscalYearGroupSum = fiscalYearDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var operatingUnitDim = facts.dimension(function (d) { return d.operatingUnit; });
-    var operatingUnitGroupSum = operatingUnitDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var agencyDim = facts.dimension(function (d) { return d.agency; });
-    var agencyGroupSum = agencyDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var categoryDim = facts.dimension(function (d) { return d.category; });
-    var categoryGroupSum = categoryDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var sectorDim = facts.dimension(function (d) { return d.sector; });
-    var sectorGroupSum = sectorDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var appropriationTypeDim = facts.dimension(function (d) {
-        if (d.fiscalYearType.indexOf("Base") != -1)
-            return "Base";
-        if (d.fiscalYearType.indexOf("Supp") != -1)
-            return "Supplemental";
-        return "Request";
-    });
-    var appropriationTypeGroupSum = appropriationTypeDim.group().reduceSum(function (fact) { return fact.amount; });
-
-    var lightBlue = ['#9ecae1'];
-
-
-    var operatingUnitChart = dc.rowChart("#dc-operating-unit-row-chart");
-    var agencyChart = dc.rowChart("#dc-agency-row-chart");
-    var categoryChart = dc.rowChart("#dc-category-row-chart");
-    var sectorChart = dc.rowChart("#dc-sector-row-chart");
-    var appropriationTypeChart = dc.pieChart("#dc-appropriation-type-pie-chart");
-
-    dc.barChart("#dc-fiscal-year-bar-chart")
+                   
+    dc.barChart("#dc-chart-fiscalYear")
         .width(700) // bootstrap default is 1170
         .height(200).margins({ top: 10, right: 10, bottom: 20, left: 100 })
         .dimension(fiscalYearDim)
@@ -61,7 +30,17 @@ d3.csv("data/foreignAssistance.csv", function (data) {
         .elasticY(true)
         .xAxis().tickFormat(d3.format("d")); // No commas for thousands
 
-    appropriationTypeChart
+    var appropriationTypeDim = facts.dimension(function (d) {
+        if (d.fiscalYearType.indexOf("Base") != -1)
+            return "Base";
+        if (d.fiscalYearType.indexOf("Supp") != -1)
+            return "Supplemental";
+        return "Request";
+    });
+
+    var appropriationTypeGroupSum = appropriationTypeDim.group().reduceSum(function (fact) { return fact.amount; });
+
+    dc.pieChart("#dc-chart-appropriationType")
         .width(200)
         .height(200)
         .radius(80)
@@ -69,49 +48,31 @@ d3.csv("data/foreignAssistance.csv", function (data) {
         .dimension(appropriationTypeDim)
         .group(appropriationTypeGroupSum);
 
-    operatingUnitChart
-        .width(250)
-        .height(1000).margins({ top: 0, right: 10, bottom: 20, left: 10 })
-        .dimension(operatingUnitDim)
-        .group(operatingUnitGroupSum)
-        .transitionDuration(500)
-        .data(function (measure) { return operatingUnitGroupSum.top(40); })
-        .ordinalColors(lightBlue)
-        .elasticX(true)
-        .xAxis().tickFormat(d3.format(".2s")); // Billioms
-
-    agencyChart
-        .width(250)
-        .height(200).margins({ top: 0, right: 10, bottom: 20, left: 10 })
-        .dimension(agencyDim)
-        .group(agencyGroupSum)
-        .transitionDuration(500)
-        .data(function (measure) { return agencyGroupSum.top(40); })
-        .ordinalColors(lightBlue)
-        .elasticX(true)
-        .xAxis().tickFormat(d3.format(".2s"));
-
-    categoryChart
-        .width(250)
-        .height(300).margins({ top: 0, right: 10, bottom: 20, left: 10 })
-        .dimension(categoryDim)
-        .group(categoryGroupSum)
-        .transitionDuration(500)
-        .data(function (measure) { return categoryGroupSum.top(40); })
-        .ordinalColors(lightBlue)
-        .elasticX(true)
-        .xAxis().tickFormat(d3.format(".2s"));
-
-    sectorChart
-        .width(250)
-        .height(1000).margins({ top: 0, right: 10, bottom: 20, left: 10 })
-        .dimension(sectorDim)
-        .group(sectorGroupSum)
-        .transitionDuration(500)
-        .data(function (measure) { return sectorGroupSum.top(40); })
-        .ordinalColors(lightBlue)
-        .elasticX(true)
-        .xAxis().tickFormat(d3.format(".2s"));
+    new RowChart(facts, "operatingUnit", "amount", 250, 10);
+    new RowChart(facts, "agency", "amount", 250, 10);
+    new RowChart(facts, "category", "amount", 250, 10);
+    new RowChart(facts, "sector", "amount", 250, 10);
 
     dc.renderAll();
 });
+
+
+var RowChart = function (facts, attribute, measure, width, maxItems) {
+    this.dim = facts.dimension(dc.pluck(attribute));
+    this.group = this.dim.group().reduceSum(function (d) { return d.amount; });
+
+    dc.rowChart("#dc-chart-" + attribute)
+        .dimension(this.dim)
+        .group(this.group)
+        .data(function (measure) { return measure.top(maxItems); })
+        .width(width)
+        .height(maxItems * 30)
+        .margins({ top: 0, right: 10, bottom: 20, left: 10 })
+        .elasticX(true)
+        .ordinalColors(['#9ecae1'])
+        .labelOffsetX(5)
+        //.label(function (d) { return "!!"; }) // return dashboard.label(d, that.chart, dashboard.all, lookupDimension); })
+        //.title(function (d) { return "!!"; }) //dashboard.title(d, that.chart, dashboard.all, lookupDimension); })
+        .xAxis().ticks(4).tickFormat(d3.format(".2s"));
+    debugger;
+}
