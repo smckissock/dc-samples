4f4fde249e60ed920ef83a70c778131296e0d9f9
Scott McKissock <smckissock@gmail.com>
2014-06-22 07:23:17 -0400

    Request now shows in stacked bar chart

--- foreign-assistance/js/foreignAssistance.js
+++ foreign-assistance/js/foreignAssistance.js
@@ -1,5 +1,4 @@
 
-'use strict';
 
 d3.csv("data/foreignAssistance.csv", function (data) {
     data.forEach(function (d) {
@@ -30,46 +29,62 @@ d3.csv("data/foreignAssistance.csv", function (data) {
             return d / billion; 
         })
         .formatNumber(function (d) { return Math.round(d) + " Billion"; });
+
+    var appropriationTypeColors =
+         ["#74C365", // light green 
+         "#006600",  // dark green 
+         "#007BA7"]; // blue
+         
         
     var appropriationTypeDim = facts.dimension(dc.pluck('appropriationType'));
-    var appropriationTypeGroupSum = appropriationTypeDim.group().reduceSum(
-        function (fact) { return fact.amount; });
-    dc.pieChart("#dc-chart-appropriationType")
+    var appropriationTypeGroupSum =
+        appropriationTypeDim.group().reduceSum(dc.pluck("amount"));
+    var pie = dc.pieChart("#dc-chart-appropriationType")
         .width(200)
         .height(200)
         .radius(80)
         .dimension(appropriationTypeDim)
-        .group(appropriationTypeGroupSum);
+        .group(appropriationTypeGroupSum)
+        .ordinalColors(appropriationTypeColors)
+        .colorAccessor(function (d) {
+            debugger;
+            if (d.key == "Base") return 0;
+            else if (d.key == "Supplemental") return 1
+            else return 2;
+        });
+
 
     var fiscalYearDim = facts.dimension(dc.pluck('fiscalYear'));
     var fiscalYearGroupSum = fiscalYearDim.group().reduce(
         function (p, v) {
-            p[v.appropriationType] = (p[v.appropriationType] || 0) + v.amount;
+            p[v.appropriationType] = (p[v.appropriationType] || 0) + +v.amount;
             return p;
         },
         function (p, v) {
-            p[v.appropriationType] = (p[v.appropriationType] || 0) - v.amount;
+            p[v.appropriationType] = (p[v.appropriationType] || 0) - +v.amount;
             return p;
         },
-        function () { return {}; }
+        function () { return {"Base": 0, "Supplemental": 0, "Request": 0 }; }
     );
 
     var bar = dc.barChart("#dc-chart-fiscalYear")
-        .width(650) // bootstrap default is 1170
+        .width(650) 
         .height(200).margins({ top: 10, right: 30, bottom: 20, left: 50 })
         .dimension(fiscalYearDim)
-        .group(fiscalYearGroupSum, "Base").valueAccessor(function (d) { return d.value["Base"]; })
-        .stack(fiscalYearGroupSum, "Supplemental", function (d) { return d.value["Supplemental"]; })
-        .stack(fiscalYearGroupSum, "Request", function (d) { return d.value["Request"]; })
-        .transitionDuration(500)
+        .group(fiscalYearGroupSum, "Base").valueAccessor(function (d) { return d.value.Base; })
+        .stack(fiscalYearGroupSum, "Supplemental", function (d) { return d.value.Supplemental; })
+        .stack(fiscalYearGroupSum, "Request", function (d) { return d.value.Request; })
         .legend(dc.legend().x(60).y(20))
+        .gap(10)  // space between bars
         .centerBar(true)
-        .gap(10)  // Gap between bars
-        .filter([2005.5, 2015.5]) 
+        .filter([2005.5, 2015.5])
         .x(d3.scale.linear().domain([2005.5, 2015.5]))
-        .elasticY(true);
+        .elasticY(true)
+        .ordinalColors(appropriationTypeColors);
 
-    // these don't return the chart, so can't chain them 
+    // These don't return the chart, so can't chain them 
+
+    // Years: need "2005" not "2,005" 
     bar.xAxis().tickFormat(d3.format("d"));
     bar.yAxis().tickFormat(function (v) { return v / billion + " B"; });
 
@@ -82,13 +97,13 @@ d3.csv("data/foreignAssistance.csv", function (data) {
     dc.renderAll();
 });
 
+
 var RowChart = function (facts, attribute, width, maxItems) {
     this.dim = facts.dimension(dc.pluck(attribute));
-    this.group = this.dim.group().reduceSum(function (d) { return d.amount; });
 
     dc.rowChart("#dc-chart-" + attribute)
         .dimension(this.dim)
-        .group(this.group)
+        .group(this.dim.group().reduceSum(dc.pluck("amount")))
         .data(function(measure) { return measure.top(maxItems); })
         .width(width)
         .height(maxItems * 22)
